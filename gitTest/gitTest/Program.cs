c70255ebfd26131ecc5df3a88fcf2dbd92d6f908
Tagreeg <vovosel2413@gmail.com>
2022-03-05 11:55:40 +0500

    pull

---
+++ gitTest/gitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("12143");
+            Console.ReadKey();
+        }
+    }
+}
